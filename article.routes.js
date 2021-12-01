d0649108e87f5af5f7c876b5c391fcb6a6b5cd7e
Алина <@DESKTOP-306NSDC>
2021-12-01 12:51:17 +0200

    working

--- article.routes.js
+++ article.routes.js
@@ -1,9 +1,11 @@
+const articles = require("./article.controller.js");
 
 module.exports = app => {
     const articles = require("./article.controller.js");
 
     app.post("/articles", articles.create);
     app.get("/articles", articles.findAll);
+    app.get("/articles2", articles.findAll);
     app.get("/articles/:articleId", articles.findOne);
     app.put("/articles/:articleId", articles.update);
     app.delete("/articles/:articleId", articles.delete);
--- config/db.config.js
+++ config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     HOST: "localhost",
     USER: "root",
-    PASSWORD: "root",
+    PASSWORD: "ZfJ75gPhsKrHEej5",
     DB: "articles" +
         ""
 };
